4c621854ec2759d1c7570d390d30faf37af000fb
JIdayyy <julien_abbadie@hotmail.fr>
2021-07-24 17:16:59 +0200

    card style update

--- components/Blocs/SkillCard.tsx
+++ components/Blocs/SkillCard.tsx
@@ -28,13 +28,13 @@ export default function SkillCard({ title }: IProps): JSX.Element {
       : "";
 
   return (
-    <div className="lg:w-60 items-start align-middle my-4 w-full text-gray-500 flex  flex-col ">
+    <div className="lg:w-60 border  shadow-lg items-start align-middle my-4 w-full text-gray-500 flex  flex-col ">
       <img
         className="w-40 px-4 lg:px-0 h-40 lg:w-60 lg:h-60"
         src={image}
         alt=""
       />
-      <div className="flex flex-col px-4 lg:px-0">
+      <div className="flex flex-col px-4 ">
         <h1 className="text-gray-700 my-2">{title}</h1>
         <div className="">{body}</div>
       </div>
